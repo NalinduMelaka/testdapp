f5711014200840954e37bc4e2d7e80c23555f2e7
nalindu <nalindumelaka@outlook.com>
2024-04-18 15:38:53 +0530

    base

--- src/dapp_frontend/src/main.jsx
+++ src/dapp_frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss';
+import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
